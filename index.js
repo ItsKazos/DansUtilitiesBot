36865a2ec1542a0e105deb1d1c371ed7200479f9
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-10 21:20:36 -0400

    First commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on("message", async message => {
         ) {
             const target = mentions.users.first()
             if (target) {
-                member.send(`tes`)
+                target.send(`tes`)
                 const targetMember = message.guild.members.cache.get(target.id)
                 targetMember.kick(`Kicked by: ${member.name}`)
                 message.channel.send(`${targetMember} has been kicked.`)
