247a6955fef863d23ce23dc66ef7c614b79b01c9
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-10 20:19:07 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+const config = require("./config.json")
+const bot = new discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is ready for action!`);
+    bot.user.setActivity("I see you :eyes:");
+});
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}test`) {
+        return message.channel.send("Hello World");
+    }
+});
+
+bot.login (config.token);
