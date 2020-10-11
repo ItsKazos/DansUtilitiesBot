c23f2e41a02694e805c002e0b44fea5c84761e7f
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-11 14:31:23 -0400

    First commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ bot.on("message", async message => {
     if (cmd === `${prefix}ping`) {
         if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to execute this command!")
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-        if (!user) return message.channel.send(`You did not mention a user or provide an ID!`).
+        if (!user) return message.channel.send(`You did not mention a user or provide an ID!`);
         if(!args.slice(1).join(" ")) return message.channel.send("You did not specify your message");
         user.user.send(args.slice(1).join()).catch(() => message.channel.send("That member has their dms disabled!")).then(() => message.channel.send(`Sent a message to <@${user.id}>`))
     }
