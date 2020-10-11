cd0fd15178cdb04d5fb7d2a5910f5f804619ec86
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-11 14:50:58 -0400

    First commit

--- index.js
+++ index.js
@@ -17,7 +17,35 @@ bot.on("message", async message => {
     let args = messageArray.slice(1);
 
     if (cmd === `${prefix}test`) {
-        return message.channel.send(`Test complete!`);
+        message.channel.send({embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL()
+            },
+            title: "This is an embed",
+            url: "http://google.com",
+            description: "This is a test embed to showcase what they look like and what they can do.",
+            fields: [{
+                name: "Fields",
+                value: "They can have different fields with small headlines."
+              },
+              {
+                name: "Masked links",
+                value: "You can put [masked links](http://google.com) inside of rich embeds."
+              },
+              {
+                name: "Markdown",
+                value: "You can put all the *usual* **__Markdown__** inside of them."
+              }
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL(),
+              text: "© Example"
+            }
+          }
+        });
     }
     if (cmd === `${prefix}ping`) {
         if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to execute this command!")
