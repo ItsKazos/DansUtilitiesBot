12ee5e814315d7f29e059169954626abe9ba7391
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-10 21:30:27 -0400

    First commit

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ bot.on("message", async message => {
         ) {
             const target = mentions.users.first()
             if (target) {
+                msg.channel.type === (`"dm"`) + msg.target.send(`"Your other message"`);
                 const targetMember = message.guild.members.cache.get(target.id)
-                targetMember.kick(`Kicked by: ${member.name}`)
+                targetMember.kick()
                 message.channel.send(`${targetMember} has been kicked.`)
             } else{
                 message.channel.send(`<@${member.id}>, please specify a player to kick from the server.`)
