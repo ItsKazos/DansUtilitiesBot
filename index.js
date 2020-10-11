f225ee1a96691881bc7587b5381cac5cbff0007c
ItsMeDan17 <itsmedan177@gmail.com>
2020-10-11 14:40:11 -0400

    First commit

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ bot.on("message", async message => {
             member.hasPermission('ADMINISTRATOR') ||
             member.hasPermission('KICK_MEMBERS')
         ) {
-            const target = mentions.users.first()
-            if (target) {
+            const user = mentions.users.first()
+            const reason = args.slice(1).join(" ")
+            if (user) {
                 if(!args.slice(1).join(" ")) return message.channel.send("You did not specify the reason of this kick!");
-                const targetMember = message.guild.members.cache.get(target.id)
-                targetMember.kick()
+                const targetMember = message.guild.members.cache.get(user.id)
+                user.user.send(args.slice(1).join(" ") + `You have been kicked from Dan's Hangout! Reason: ` + args.slice(1).join(" ") + ` Issued by: <@${member.id}>`).catch(() => message.channel.send("That member has their dms disabled!"))
+                targetMember.kick(`${reason}`)
                 message.channel.send(`${targetMember} has been kicked.`)
             } else{
                 message.channel.send(`<@${member.id}>, please specify a player to kick from the server.`)
