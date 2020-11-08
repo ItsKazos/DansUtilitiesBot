ab2e98aaaab9b72a443d375304a8d84496449012
ItsMeDan17 <itsmedan177@gmail.com>
2020-11-07 21:16:51 -0500

    added stuff for dans hangout mc

--- index.js
+++ index.js
@@ -343,18 +343,34 @@ Reason: ${args.slice(1).join(" ")}`,
                             SEND_MESSAGES: false
                         });
                     })
-                    general = bot.channels.cache.get("762115463911833641");
-                    generaloverflow = bot.channels.cache.get("762115504466296852");
-                    media = bot.channels.cache.get("762115512200331314");
-                    gaming = bot.channels.cache.get("762115542269820949");
-                    questions = bot.channels.cache.get("762115595063132202");
-                    otherlanguages = bot.channels.cache.get("762443522397306920");
-                    botcommands = bot.channels.cache.get("762154748530130984");
-                    appealmute = bot.channels.cache.get("762163187302137866");
-                    dankmemer = bot.channels.cache.get("770747807068848138");
-                    dankmemer2 = bot.channels.cache.get("771426330444693534");
-                    selfadvertising = bot.channels.cache.get("762116284342206464");
-                    voicetext = bot.channels.cache.get("762520920685805588");
+                    if(message.guild.id == `774806096019324938`) {
+                        general = bot.channels.cache.get("774806096623304736");
+                        generaloverflow = bot.channels.cache.get("774806096623304737");
+                        media = bot.channels.cache.get("774806096623304738");
+                        gaming = bot.channels.cache.get("774806096623304739");
+                        questions = bot.channels.cache.get("774806096623304740");
+                        otherlanguages = bot.channels.cache.get("774806096623304741");
+                        botcommands = bot.channels.cache.get("774806096623304743");
+                        appealmute = bot.channels.cache.get("774806096807198750");
+                        dankmemer = bot.channels.cache.get("774813652938915850");
+                        dankmemer2 = bot.channels.cache.get("774816409133645895");
+                        selfadvertising = bot.channels.cache.get("774806097403838481");
+                        voicetext = bot.channels.cache.get("774806097982259211");
+                    } else {
+                        general = bot.channels.cache.get("762115463911833641");
+                        generaloverflow = bot.channels.cache.get("762115504466296852");
+                        media = bot.channels.cache.get("762115512200331314");
+                        gaming = bot.channels.cache.get("762115542269820949");
+                        questions = bot.channels.cache.get("762115595063132202");
+                        otherlanguages = bot.channels.cache.get("762443522397306920");
+                        botcommands = bot.channels.cache.get("762154748530130984");
+                        appealmute = bot.channels.cache.get("762163187302137866");
+                        dankmemer = bot.channels.cache.get("770747807068848138");
+                        dankmemer2 = bot.channels.cache.get("771426330444693534");
+                        selfadvertising = bot.channels.cache.get("762116284342206464");
+                        voicetext = bot.channels.cache.get("762520920685805588");
+
+                    }
                     general.send(`:oncoming_police_car: All channels have been locked! :oncoming_police_car:
 Refer to <#${message.channel.id}>!`)
                     generaloverflow.send(`:oncoming_police_car: All channels have been locked! :oncoming_police_car:
