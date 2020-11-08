37841cc3958a367a6eb156064a8c634dd75c3054
ItsMeDan17 <itsmedan177@gmail.com>
2020-11-08 10:51:59 -0500

    added >addping command

--- index.js
+++ index.js
@@ -35,8 +35,36 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+    if (cmd === `${prefix}addping`) {
+        const { member, mentions } = message
+        if(message.guild.id === `762104108261572610`) {
+            if (args[0]) {
+                if (args[0] === `giveaways`) {
+                    let role = message.guild.roles.cache.find(r => r.name === "Giveaway Ping");
+                    member.roles.add(role)
+                    message.channel.send("<@" + member.id + ">, added the role ``Giveaway Ping`` to you.")
+                } else {
+                    if (args[0] === `announcements`) {
+                        let role = message.guild.roles.cache.find(r => r.name === "All Announcements");
+                        member.roles.add(role)
+                        message.channel.send("<@" + member.id + ">, added the role ``All Announcements`` to you.")
+                    } else {
+                        if (args[0] === `staffupdates`) {
+                            let role = message.guild.roles.cache.find(r => r.name === "Staff Updates");
+                            member.roles.add(role)
+                            message.channel.send("<@" + member.id + ">, added the role ``Staff Updates`` to you.")
+                        } else {
+                            message.channel.send("<@" + member.id + ">, please type ``" + prefix + "addping [giveaways | announcements | staffupdates]``, ``" + args[0] + "`` did not match any of the following.")
+                        }
+                    }
+                }
+            }
+        } else {
+            message.channel.send("<@" + member.id + ">, you can't use this command in ``" + message.guild.name + " server!``, This command requires to be used in ``Dan's Hangout``.")
+        }
+    }
 
-    if (message.channel.id === `774816409133645895`) {
+    if (message.channel.id === `774816409133645895` || message.channel.id === `762110786335277066`) {
         if(cmd === `${prefix}verify`) {
             if (args[0] === `E39h2kg3A`) {
                 const { member, mentions } = message
